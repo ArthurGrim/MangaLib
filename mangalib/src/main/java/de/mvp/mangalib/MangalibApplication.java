ea247979a0e4fb5229d3d10abd5c18b69acf03c8
ArthurGrim <92574451+ArthurGrim@users.noreply.github.com>
2024-02-16 19:20:41 +0100

    Add files via upload
    Die Datein die ich durch Spring Initializr erhalten habe

---
+++ mangalib/src/main/java/de/mvp/mangalib/MangalibApplication.java
@@ -0,0 +1,13 @@
+package de.mvp.mangalib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangalibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangalibApplication.class, args);
+	}
+
+}
