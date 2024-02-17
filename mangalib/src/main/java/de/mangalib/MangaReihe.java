5a71436410004579c3403910afaf652d7f55acfd
ArthurGrim <blitzda16@gmail.com>
2024-02-17 17:59:03 +0100

    Adding Class MangaReihe and some minor changes like editing the project path

---
+++ mangalib/src/main/java/de/mangalib/MangaReihe.java
@@ -0,0 +1,58 @@
+package de.mangalib;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "mangareihe")
+@Getter @Setter @NoArgsConstructor
+public class MangaReihe {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "mangaIndex")
+    private Integer mangaIndex;
+
+    @Column(name = "statusID")
+    private Integer statusID;
+
+    @Column(name = "verlagID")
+    private Integer verlagID;
+
+    @Column(name = "typID")
+    private Integer typID;
+
+    @Column(name = "formatID")
+    private Integer formatID;
+
+    @Column(name = "titel")
+    private String titel;
+
+    @Column(name = "anzahlBaende")
+    private Integer anzahlBaende;
+
+    @Column(name = "preisProBand")
+    private Double preisProBand;
+
+    @Column(name = "gesamtpreis")
+    private Double gesamtpreis;
+
+    @Column(name = "erstelltAm")
+    private java.sql.Timestamp erstelltAm;
+
+    @Column(name = "aktualisiertAm")
+    private java.sql.Timestamp aktualisiertAm;
+
+    @Column(name = "istEbayPreis")
+    private Boolean istEbayPreis;
+
+    @Column(name = "istVergriffen")
+    private Boolean istVergriffen;
+
+}
+
+
