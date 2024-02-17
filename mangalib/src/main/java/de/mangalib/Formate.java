af25e83636b1a7f99cfd93fac06b6a33adc1aaed
ArthurGrim <blitzda16@gmail.com>
2024-02-17 18:20:11 +0100

    Creating Class Formate

---
+++ mangalib/src/main/java/de/mangalib/Formate.java
@@ -0,0 +1,20 @@
+package de.mangalib;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "formate")
+@Getter @Setter @NoArgsConstructor
+public class Formate {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long formatID;
+
+    @Column(name = "bezeichnung")
+    private String bezeichnung;
+
+}
