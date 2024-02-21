0ae5124b40526104bc9ac8e0a5978d03b902e801
ArthurGrim <blitzda16@gmail.com>
2024-02-21 18:40:52 +0100

    Added MyControler class to my Project. Integrated Thymleaf into HTML templates for dynamic content rendering

---
+++ mangalib/src/main/java/de/mangalib/controller/MyController.java
@@ -0,0 +1,46 @@
+package de.mangalib.controller;
+
+import de.mangalib.entity.Status;
+import de.mangalib.entity.Verlag;
+import de.mangalib.entity.Typ;
+import de.mangalib.entity.Format;
+import de.mangalib.service.StatusService;
+import de.mangalib.service.VerlagService;
+import de.mangalib.service.TypService;
+import de.mangalib.service.FormatService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import java.util.List;
+
+@Controller
+public class MyController {
+
+    @Autowired
+    private StatusService statusService;
+
+    @Autowired
+    private VerlagService verlagService;
+
+    @Autowired
+    private TypService typService;
+
+    @Autowired
+    private FormatService formatService;
+
+    @GetMapping("/meineSeite")
+    public String meineSeite(Model model) {
+        List<Status> alleStatus = statusService.findAll();
+        List<Verlag> alleVerlage = verlagService.findAll();
+        List<Typ> alleTypen = typService.findAll();
+        List<Format> alleFormate = formatService.findAll();
+
+        model.addAttribute("alleStatus", alleStatus);
+        model.addAttribute("alleVerlage", alleVerlage);
+        model.addAttribute("alleTypen", alleTypen);
+        model.addAttribute("alleFormate", alleFormate);
+
+        return "home"; // Name der HTML-Datei ohne .html
+    }
+}
