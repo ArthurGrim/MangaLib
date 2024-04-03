d6976e253eaa4af9c965fc9cc963135f698dfa8e
ArthurGrim <blitzda16@gmail.com>
2024-04-03 12:37:25 +0200

    Created Subpages for the Statistik Page with basic Framework

--- mangalib/src/main/java/de/mangalib/controller/StatistikController.java
+++ mangalib/src/main/java/de/mangalib/controller/StatistikController.java
@@ -7,11 +7,48 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class StatistikController {
 
-    @GetMapping("/statistik")
-    public String statistik(Model model) {
+    // Methode um die Statistik Overview Seite aufzurufen
+    @GetMapping("/statistik/st_overview")
+    public String statistikOverview(Model model) {
         // Hier können Sie Model-Attribute hinzufügen, die auf der Seite benötigt werden
         // Beispiel: model.addAttribute("attributName", attributWert);
 
-        return "statistik"; // Name der Thymeleaf-Template-Datei ohne die .html-Erweiterung
+        return "statistik/st_overview"; // Name der Thymeleaf-Template-Datei ohne die .html-Erweiterung
+    }
+
+    // Methode um die Statistik Status Seite aufzurufen
+    @GetMapping("/statistik/st_status")
+    public String statistikStatus(Model model) {
+        // Hier können Sie Model-Attribute hinzufügen, die auf der Seite benötigt werden
+        // Beispiel: model.addAttribute("attributName", attributWert);
+
+        return "statistik/status"; // Name der Thymeleaf-Template-Datei ohne die .html-Erweiterung
+    }
+
+    // Methode um die Statistik Verlag Seite aufzurufen
+    @GetMapping("/statistik/st_verlag")
+    public String statistikVerlag(Model model) {
+        // Hier können Sie Model-Attribute hinzufügen, die auf der Seite benötigt werden
+        // Beispiel: model.addAttribute("attributName", attributWert);
+
+        return "statistik/st_verlag"; // Name der Thymeleaf-Template-Datei ohne die .html-Erweiterung
+    }
+
+    // Methode um die Statistik Typ Seite aufzurufen
+    @GetMapping("/statistik/st_typ")
+    public String statistikTyp(Model model) {
+        // Hier können Sie Model-Attribute hinzufügen, die auf der Seite benötigt werden
+        // Beispiel: model.addAttribute("attributName", attributWert);
+
+        return "statistik/st_typ"; // Name der Thymeleaf-Template-Datei ohne die .html-Erweiterung
+    }
+
+    // Methode um die Statistik Format Seite aufzurufen
+    @GetMapping("/statistik/st_format")
+    public String statistikFormat(Model model) {
+        // Hier können Sie Model-Attribute hinzufügen, die auf der Seite benötigt werden
+        // Beispiel: model.addAttribute("attributName", attributWert);
+
+        return "statistik/st_format"; // Name der Thymeleaf-Template-Datei ohne die .html-Erweiterung
     }
 }
